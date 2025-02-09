d063bea34609a6321f9ba3fbb26ad7227d350dfd
ma_frolov <ma.frolov@tensor.ru>
2025-02-09 21:09:35 +0300

    go to tensor.ru/about

--- tensor_test.py
+++ tensor_test.py
@@ -13,20 +13,26 @@ try:
     # Открываем страницу
     url = ('https://sbis.ru/')
     driver.get(url)
-    # time.sleep(2)
+    time.sleep(2)
     link_text = driver.find_element(By.LINK_TEXT, "Контакты")
     link = link_text.get_attribute("href")
     driver.get(link)
-    # time.sleep(2)
+    time.sleep(2)
     banner = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div[1]/div[2]/div[1]/div/div/div[1]/div/div[4]/div[1]/div/div/div[2]/div/a')
     banner.click()
-    # time.sleep(2)
+    time.sleep(2)
     tabs = driver.window_handles
     driver.switch_to.window(tabs[1])
     try:
-        element = driver.find_element(By.XPATH, '//*[contains(text(), "Сила в людях")]')
-        print(element.text)
-        print(element.__dict__)
+        element1 = driver.find_element(By.XPATH, '//*[text()="Сила в людях"]')
+        print(element1.text)
+    except Exception as err:
+        print(repr(err))
+    try:
+        element2 = element1.find_element(By.XPATH, './/ancestor::div/p/a[contains(text(), "Подробнее")]')
+        link = element2.get_attribute("href")
+        time.sleep(2)
+        driver.get(link)
     except Exception as err:
         print(repr(err))
 
